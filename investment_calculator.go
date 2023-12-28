2ab7639d041cae543b916492520c5770daabfa13
dethblo <dethblo@live.com>
2023-12-28 17:54:57 -0500

    Re-factored to a multi-value return function.

--- investment_calculator.go
+++ investment_calculator.go
@@ -5,8 +5,9 @@ import (
 	"math"
 )
 
+const inflationRate = 2.5
+
 func main() {
-	const inflationRate = 2.5
 	var investmentAmount float64
 	var years float64
 	expectedReturnRate := 5.5
@@ -20,8 +21,7 @@ func main() {
 	fmt.Print("Years: ")
 	fmt.Scan(&years)
 
-	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
 
 	// basic approach
 	//fmt.Println("Future Value: ", futureValue)
@@ -36,4 +36,10 @@ func main() {
 Future Value: %.2f
 Future Value (adjusted for inflation): %.2f
 	`, futureValue, futureRealValue)
+}
+
+func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (float64, float64) {
+	fv := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	rfv := fv / math.Pow(1+inflationRate/100, years)
+	return fv, rfv
 }
